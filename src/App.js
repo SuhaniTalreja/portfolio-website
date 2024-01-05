fd56458901fef97b091d91ad7f7f2bfdb1600d5a
Suhani Talreja <suhani.talreja.29@gmail.com>
2024-01-05 12:31:41 +0530

    project addition

--- src/App.js
+++ src/App.js
@@ -10,27 +10,30 @@ import Education from './Components/Education';
 import Blog from './Components/Blog';
 import Projects from './Components/Projects';
 import Footer from './Components/Footer';
-
+import CssBaseline from '@mui/material/CssBaseline';
 
   const StyledComponent = styled(Box)`
     font-family: 'Poppins';
-    width: 100%;
+    width: 100vw;
     height: 100%;
     margin: 0;
     background: linear-gradient(to bottom, rgba(253, 143, 82, 0.5), rgba(255, 184, 113, 0.5), rgba(255, 220, 162, 0.5));
 `
 
+
 function App() {
   return (
     <StyledComponent>
+    <CssBaseline>
       <Navbar />
-      <About id="about"/>
-      <Skills />
-      <Experience />
-      <Projects />
-      <Blog />
-      <Education />
-      <Footer />
+        <About id="about"/>
+        <Skills />
+        <Experience />
+        <Projects />
+        <Blog />
+        <Education />
+        <Footer />
+        </CssBaseline>
     </StyledComponent>
   );
 }
--- src/Components/Utils/projectSet.jsx
+++ src/Components/Utils/projectSet.jsx
@@ -2,68 +2,14 @@ export const projects = [
     {
       id: 0,
       title: "1",
-      date: "29 january",
-      desc:"this night is flawless dont you let it go, im wonderstruck blushing all the way home.this night is flawless dont you let it go, im wonderstruck blushing all the way home",
+      date: "5 January",
+      desc:"I have always been a fan of good movies, sitcoms, thrillers, and dramas. As a fan, I decided to work on a project related to my favorite application—Netflix! This endeavor allowed me to delve into backend development, integrate APIs, handle events, and understand the intricacies of linking and routing.",
       image:
-        "https://media.glamour.com/photos/643991c1cf4aa0388cf3499e/master/w_2560%2Cc_limit/1482125734",
-      tags: ["hi","hi","hi"],
+        "https://e1.pxfuel.com/desktop-wallpaper/1001/320/desktop-wallpaper-netflix-logo-transparent-png-netflix-2021-thumbnail.jpg",
+      tags: ["React","Firebase","TMDB API"],
       category: "react",
-      github: "https://www.linkedin.com/feed/",
-      webapp: "https://people.com/thmb/CDxHoM8fia5x_Fdg5KGrZHolT_0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x69:721x71)/taylor-swift-120522-e4afb2bc18f74494bf2754b040883358.jpg",
+      github: "https://github.com/SuhaniTalreja/netflix-clone",
+      webapp: "https://netflix-clone2-61b2c.web.app/",
     },
-    {
-        id: 1,
-        title: "2",
-        date: "29 january",
-        desc:
-        "this night is flawless dont you let it go, im wonderstruck blushing all the way home.this night is flawless dont you let it go, im wonderstruck blushing all the way home",
-        image:
-          "https://www.rollingstone.com/wp-content/uploads/2023/04/taylor-swift-city-celebrations.jpg?w=1581&h=1054&crop=1",
-      tags: ["hi","hi","hi"],
-        category: "react",
-        github: "https://www.linkedin.com/feed/",
-        webapp: "https://people.com/thmb/CDxHoM8fia5x_Fdg5KGrZHolT_0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x69:721x71)/taylor-swift-120522-e4afb2bc18f74494bf2754b040883358.jpg",
-    },
-    {
-        id: 2,
-        title: "3",
-        date: "29 january",
-        desc:
-        "this night is flawless dont you let it go, im wonderstruck blushing all the way home.this night is flawless dont you let it go, im wonderstruck blushing all the way home",
-        image:
-          "https://people.com/thmb/CDxHoM8fia5x_Fdg5KGrZHolT_0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x69:721x71)/taylor-swift-120522-e4afb2bc18f74494bf2754b040883358.jpg",
-      tags: ["hi","hi","hi"],
-        category: "javascript",
-        github: "https://www.linkedin.com/feed/",
-        webapp: "https://people.com/thmb/CDxHoM8fia5x_Fdg5KGrZHolT_0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x69:721x71)/taylor-swift-120522-e4afb2bc18f74494bf2754b040883358.jpg",
-    },
-
-    {
-        id: 3,
-        title: "4",
-        date: "29 january",
-        desc:
-        "this night is flawless dont you let it go, im wonderstruck blushing all the way home.this night is flawless dont you let it go, im wonderstruck blushing all the way home",
-        image:
-          "https://i.guim.co.uk/img/media/ffe382ed7e6b9ac76064bece37622113576dfd87/0_30_5606_3363/master/5606.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=ea4fcd3db26db255eb6a90b142803b1e",
-      tags: ["hi","hi","hi"],
-        category: "misc",
-        github: "https://www.linkedin.com/feed/",
-        webapp: "https://people.com/thmb/CDxHoM8fia5x_Fdg5KGrZHolT_0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x69:721x71)/taylor-swift-120522-e4afb2bc18f74494bf2754b040883358.jpg",
-    },
-    {
-      id: 4,
-      title: "5",
-      date: "29 january",
-      desc:
-        "this night is flawless dont you let it go, im wonderstruck blushing all the way home.this night is flawless dont you let it go, im wonderstruck blushing all the way home",
-      image:
-        "https://assets.vogue.com/photos/6579bb0ab8233ad7c411a7e3/master/pass/TS_beauty_.jpg",
-      tags: ["hi","hi","hi"],
-      category: "misc",
-      github: "https://www.linkedin.com/feed/",
-      webapp: "https://people.com/thmb/CDxHoM8fia5x_Fdg5KGrZHolT_0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x69:721x71)/taylor-swift-120522-e4afb2bc18f74494bf2754b040883358.jpg",
-  },
-
     
   ];
